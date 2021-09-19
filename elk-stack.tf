dcd91c98e887fcfadf6065b299398a26d69eb872
ezeutno <ivan.suratno@gmail.com>
2021-09-19 12:24:58 +0700

    Install Elasticsearch using Remote-Exec

--- elk-stack.tf
+++ elk-stack.tf
@@ -1,3 +1,4 @@
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/rpm.html
 resource "aws_instance" "elasticsearch" {
   # due to aws limitation, we can't use ubuntu and must use amazon ami which is based on centos   
   ami           = "ami-087c17d1fe0178315" # us-west-1
@@ -12,6 +13,19 @@ resource "aws_instance" "elasticsearch" {
   }
 
   vpc_security_group_ids = [ aws_security_group.allow_access_to_system.id ]
+
+  provisioner "file" {
+    source      = "file/elasticsearch.repo"
+    destination = "/tmp/elasticsearch.repo"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch",
+      "sudo mv /tmp/elasticsearch.repo /etc/yum.repos.d/elasticsearch.repo",
+      "sudo yum install --enablerepo=elasticsearch elasticsearch",
+    ]
+  }
 
   tags = {
     Name = "Elasticsearch"
