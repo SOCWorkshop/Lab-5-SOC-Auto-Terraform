4971802e7101c9ef8d5d9149b6b8a787ee36a299
ezeutno <ivan.suratno@gmail.com>
2021-09-19 11:05:50 +0700

    Use terraform private key generator instead

--- key.tf
+++ key.tf
@@ -1,13 +1,14 @@
-resource "null_resource" "generate_ssh_key" {
-    provisioner "local-exec" {
-        command = "ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa"
-    }
+# generate private key
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "cloud9key" {
-  key_name   = "cloud9key"
-  public_key = file(pathexpand("~/.ssh/id_rsa"))
-  depends_on = [
-     null_resource.generate_ssh_key
-  ]
+  key_name   = "cloud9key"      
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ${pathexpand("~/.ssh/id_rsa")}"
+  }
 }
