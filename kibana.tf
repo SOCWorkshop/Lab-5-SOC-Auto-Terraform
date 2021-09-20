1c3c17c4cd930a68264fcb7d9e61393ab8bb97da
ezeutno <ivan.suratno@gmail.com>
2021-09-20 08:26:33 +0700

    Fix Typo

--- kibana.tf
+++ kibana.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "kibana" {
   }
 
   provisioner "file" {
-    context = data.template_file.kibana
+    content = data.template_file.kibana
     destination = "/tmp/kibana.yml"
   }
 
